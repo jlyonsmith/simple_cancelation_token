30e3cc32c2b3eef22b4f02bb5544e78d3cdfe129
John Lyon-Smith <john@lyon-smith.org>
2025-06-11 12:47:35 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Simple Cancellation Token
+
+[![coverage](https://shields.io/endpoint?url=https://raw.githubusercontent.com/jlyonsmith/simple_cancellation_token/main/coverage.json)](https://github.com/jlyonsmith/simple_cancellation_token/blob/main/coverage.json)
+[![Crates.io](https://img.shields.io/crates/v/simple_cancellation_token.svg)](https://crates.io/crates/simple_cancellation_token)
+[![Docs.rs](https://docs.rs/simple_cancellation_token/badge.svg)](https://docs.rs/simple_cancellation_token)
+
+Simple cancellation token
---
+++ benches/benchmarks.rs
@@ -0,0 +1,34 @@
+use core::fmt::Arguments;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use simple_cancellation_token::*;
+
+fn benchmark_1(arg: &str) {
+    struct TestLogger;
+
+    impl TestLogger {
+        fn new() -> TestLogger {
+            TestLogger {}
+        }
+    }
+
+    impl SimpleCancellationTokenLog for TestLogger {
+        fn output(self: &Self, _args: Arguments) {}
+        fn warning(self: &Self, _args: Arguments) {}
+        fn error(self: &Self, _args: Arguments) {}
+    }
+
+    let logger = TestLogger::new();
+    let mut tool = SimpleCancellationTokenTool::new(&logger);
+    let args: Vec<std::ffi::OsString> = vec!["".into(), arg.into()];
+
+    tool.run(args).unwrap();
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("basic test", |b| {
+        b.iter(|| benchmark_1(black_box("--help")))
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
